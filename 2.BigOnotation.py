f4b52040fc58746e4458979a2f66ef74e5a48281
Anil K. Panta <anilpanta2053@gmail.com>
2022-02-05 21:46:54 +0545

    space complexity

--- 2.BigOnotation.py
+++ 2.BigOnotation.py
@@ -17,7 +17,7 @@
 #     time = O(n)
 #     Big O time complexity of a program is in order of n 
 
-
+print("Big O notation in order of n O(n)")
 # Eg: 
 def get_squared_numbers(numbers):
     squared_numbers = []
@@ -45,7 +45,7 @@ get_squared_numbers(numbers)
 
 # by using drop constant we can represent it as,
 #     time = O(1)
-
+print("Big O notation in order of 1 O(1)")
 # Eg 
 def find_first_pe(price, eps, index):
     pe=price[index]/eps[index]
@@ -57,6 +57,8 @@ find_first_pe([200, 500, 750], [100, 230, 500], 2)
 
 
 # ----------------O(n2)----------------------
+# Time = an2 + c 
+print("Big O notation in order of n2 O(n2)")
 # Eg 
 numbers=[3,6,2,4,3,6,8,9]
 for i in range(len(numbers)):
@@ -65,3 +67,41 @@ for i in range(len(numbers)):
             print(numbers[i])
             print(" is a dublicate")
             break
+
+
+# Above examples were for time complexity 
+# space complexity also works same but a bit more reliable one 
+# Eg:
+print("Time v/s Space Complexity")
+# Search 68 from the array of numbers 
+ArrayNumbers=[4, 9, 15, 21, 34, 57, 68, 91]
+# Using Time Complexity:
+print("Time Complexity")
+for i in range(len(ArrayNumbers)):
+    if ArrayNumbers[i]==68:
+        print(i)
+
+print("Space Complexity (Binery Search)")
+average = len(ArrayNumbers)/2
+for average in range(len(ArrayNumbers)):
+    if ArrayNumbers[average]==68:
+        print(average)
+        break
+    else:
+        if ArrayNumbers[average]>68:
+            average= average + average/2
+        else:
+            average= average - average/2
+        
+
+# For Binary Search complexity
+# Iteration k=n/2^k 
+# 1=n/2^k 
+# n=2^k 
+# log2n = log22^k 
+# log2n = k*log22 
+# k = logn 
+# O(logn)
+
+# For above example 
+# k = O(logn) = log28 = log22^3 = 3*log22 = 3 iterations 
