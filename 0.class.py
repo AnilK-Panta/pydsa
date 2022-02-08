b9f03efb6ec429b8a97c2fab162d44376f85cc10
Anil K. Panta <anilpanta2053@gmail.com>
2022-02-09 00:08:46 +0545

    init

--- 0.class.py
+++ 0.class.py
@@ -7,6 +7,9 @@
 
 
 # This is the actual way 
+from pyexpat import model
+
+
 class Computer:
     def config(self):  #Method (Function) with Attribute of self
         print("I5, 10 Gen, 1TB")
@@ -20,3 +23,16 @@ Computer.config(comp2)  #Calling method of a class of object 2
 # The calling method is same for the below, which is actually seen in practice
 comp1.config() #Takes comp1 as an Argument
 comp2.config()
+
+# __init__
+class Car:
+    def __init__(self, brand, release):
+        self.b=brand
+        self.r=release
+    def config(self):
+        print("Config is:", self.b, self.r)
+model1=Car("Tesla", 5)
+model2=Car("Hyundai", 10)
+
+model1.config()
+model2.config()
